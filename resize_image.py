10c4b2ab1b1391ee8d05135556304697c6cd93b9
IlMurlocDiplomato <93687025+IlMurlocDiplomato@users.noreply.github.com>
2024-02-29 20:02:41 +0100

    Update resize_image.py
    thumbnail size defined by user

--- resize_image.py
+++ resize_image.py
@@ -4,24 +4,13 @@ import sys
 import os
 
 def convert_to_jpg(file):
-    """
-    Convert a file to JPG format.
-    """
     try:
-        # Open the image file
         img = Image.open(file)
-
-        # Convert to RGB mode (if necessary)
         if img.mode != 'RGB':
             img = img.convert('RGB')
-
-        # Change the file extension to .jpg
         file_base = os.path.splitext(file)[0]
         new_file = file_base + '.jpg'
-
-        # Save as JPG
         img.save(new_file, 'JPEG')
-
         return new_file
     except Exception as e:
         print(f"Error converting {file} to JPG:", str(e))
@@ -32,37 +21,30 @@ def main():
         print("Usage: python script.py <file(s) or folder>")
         sys.exit(1)
 
-    # Get the absolute path of the script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     output_dir = os.path.join(script_dir, "output")
 
     paths = sys.argv[1:]
 
+    thumbnail_size_input = input("Enter thumbnail size (leave blank for default 2000): ")
+    thumbnail_size = (2000, 2000) if thumbnail_size_input == '' else (int(thumbnail_size_input), int(thumbnail_size_input))
+
     for path in paths:
         if os.path.isdir(path):
-            # If it's a directory, get all files inside
             files = glob.glob(os.path.join(path, '*'))
         else:
-            # If it's a file, just use that file
             files = [path]
 
         for file in files:
             if not file.lower().endswith('.jpg'):
-                # If the file is not a JPEG, convert it to JPEG
                 new_file = convert_to_jpg(file)
                 if new_file:
                     file = new_file
 
             try:
-                # Open the image file (read-only)
                 with Image.open(file) as image:
-                    # Resize to thumbnail
-                    image.thumbnail((2000, 2000))
-
-                    # Create output directory if it doesn't exist
+                    image.thumbnail(thumbnail_size)
                     os.makedirs(output_dir, exist_ok=True)
-
-                    # Save the resized image to output directory
                     output_file = os.path.join(output_dir, os.path.basename(file))
                     image.save(output_file)
                     print(f"Image {file} successfully resized and saved as {output_file}.")
@@ -72,4 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
